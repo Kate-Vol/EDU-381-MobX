24b6da53b558366caf2ed8eabfee81813ef4da2a
Kate-Vol <vahrusheva_katya@bk.ru>
2023-04-07 14:48:10 +0300

    EDU-381-With makeAutoObservable in constructor() and observer wrapper

---
+++ edu-381-mobx/src/store/todo.js
@@ -0,0 +1,23 @@
+import {makeAutoObservable} from "mobx";
+
+class Todo {
+  todos = [
+    {id:1, title:'Go to the shop', completed: false},
+    {id:2, title:'See TV', completed: false},
+    {id:3, title:'Give Like', completed: false}]
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo)
+  }
+  removeTodo(id) {
+    this.todos = this.todos.filter(todo => todo.id !== id)
+  }
+
+  completeTodo(id) {
+    this.todos = this.todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo)
+  }
+}
+export default new Todo()
